8263edbbf9b4b48f84f1c75a367a2601e16eaf23
Lucasxz01 <lucas_f_serafim@estudante.sesisenai.org.br>
2025-10-22 13:18:04 -0300

    Site de livros

---
+++ script.js
@@ -0,0 +1,76 @@
+
+// Dados dos produtos
+const products = [
+    {
+        id: 1,
+        name: "Sofá Conforto & Essência",
+        price: 999.99,
+        image: "image/sofa.jpg"
+    },
+    {
+        id: 2,
+        name: "Quadro De Vidro Decorativo",
+        price: 550.00,
+        image: "image/quadro.jpg"
+    },
+    {
+        id: 3,
+        name: "Lustre Moderno De Cristal ",
+        price: 399.99,
+        image: "image/lustre.jpg"
+    },
+    {
+        id: 4,
+        name: "Vaso Decorativo Cerâmica",
+        price: 79.99,
+        image: "image/vaso.jpg"
+    },
+    {
+        id: 5,
+        name: "Mesa de Cozinha Quadrada",
+        price: 1159.99,
+        image: "image/mesa.jpg"
+    },
+    {
+        id: 6,
+        name: "Cômoda Decorativa Verde Oliva",
+        price: 699.99,
+        image: "image/comoda.jpg"
+    },
+    {
+        id: 7,
+        name: "Espelho Decorativo Com Led",
+        price: 400.00,
+        image: "image/espelho decorativo.jpg"
+    },
+    {
+        id: 8,
+        name: "Estante Decorativa Para Objetos",
+        price: 399.00,
+        image: "image/estante.jpg"
+    },
+    {
+        id: 9,
+        name: "Adega Moderna Para Vinhos",
+        price: 359.90,
+        image: "image/adega.jpg" 
+    },
+    {
+        id: 10,
+        name: "Kit De Luxo Para Banheiro",
+        price: 400.00,
+        image: "image/kit.jpg"
+    },
+    {
+        id: 11,
+        name: "Lixeira De Luxo Para Banheiro",
+        price: 250.00,
+        image: "image/lixeira.jpg"
+    },
+    {
+        id:12,
+        name: "Tapete Decorativo Orgânico Moderno",
+        price: 539.99,
+        image: "image/tapete decorativo.webp"  
+    }
+];
